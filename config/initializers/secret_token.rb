482b16bab3e08c520551df2740971ef4002d55e5
siciliana <siciliana@gmail.com>
2013-12-08 22:36:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OzHomepage::Application.config.secret_token = '09ad55793f5364a2a6c0b9bef571714548fa9dbf71ca1a53a4615aa590f42230ee8dc3bc3e51577bcd1b92daf2d897aba613b3d94ec9ff52a2f44b29f68f237e'
